4042d013ea0d89c6928961126aded7efcb6ced7a
dillonmohr8777 <dillonmohr8777@gmail.com>
2026-08-19 14:03:40 -0400

    Export Phase 3 contact update and post-list helpers.

--- lib/phase3/index.ts
+++ lib/phase3/index.ts
@@ -8,10 +8,17 @@ export {
   canViewProtectedProfile,
   maxUploadBytes,
   resolveEffectiveAudiences,
+  validateContacts,
   validateUploadFile,
+  visiblePostsForView,
 } from "./audiences.ts";
 export { getPhase3Client, getBridgeApiBase, isPhase3LiveApi } from "./client.ts";
-export { MockPhase3Client, mockHarborClaims, applySimulatedFailure } from "./mock-client.ts";
+export {
+  MockPhase3Client,
+  mockHarborClaims,
+  applySimulatedFailure,
+  applyUnverifiedOrganization,
+} from "./mock-client.ts";
 export { HttpPhase3Client } from "./http-client.ts";
 export {
   Phase3Error,
@@ -24,7 +31,9 @@ export {
   type CreatePostInput,
   type Phase3Client,
   type ProfileProjection,
+  type ResponsibleContact,
   type SessionClaims,
+  type UpdateContactsInput,
   type UploadIntent,
   type PostRecord,
 } from "./types.ts";
